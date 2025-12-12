c6cd5bf32542baa304a60b24d02ecd8190f46abf
pitoneverde <pitoneverde181@gmail.com>
2025-12-12 01:13:38 +0100

    API is more generic and handles raw single bytes

--- spsc_ring/spsc_ring.c
+++ spsc_ring/spsc_ring.c
@@ -1,31 +1,74 @@
 #include "spsc_ring.h"
+#include <string.h>
+#include <math.h>
+
+// stolen from linux kfifo, roundups to multiples of 64
+static inline size_t to_cache_size(size_t n)
+{
+	n--;
+	n |= n >> 1;
+	n |= n >> 2;
+	n |= n >> 4;
+	n |= n >> 8;
+	n |= n >> 16;
+	n |= n >> 32;
+	return n + 1;
+}
 
 t_spsc_ring *spsc_create(size_t size)
 {
-
+	// in c99 is posix_memalign
+	t_spsc_ring	*ring = aligned_alloc(64, sizeof(t_spsc_ring));
+	if (!ring) return NULL;
+	size = to_cache_size(size);
+	ring->buf = aligned_alloc(64, size);
+	if (!ring->buf)
+		return (free(ring), NULL);
+	ring->size = size;
+	ring->mask = size - 1;
+	atomic_init(&ring->head, 0);
+	atomic_init(&ring->tail, 0);
+	return ring;
 }
 
-void spsc_destroy(t_spsc_ring *q)
+void spsc_destroy(t_spsc_ring *r)
+{
+	if (r)
+	{
+		if (r->buf) free(r->buf);
+		free(r);
+	}
+}
+
+bool spsc_try_push(t_spsc_ring *r, unsigned char byte)
+{
+	size_t logical_size = r->size - 1;
+	size_t curr_tail = atomic_load_explicit(&r->tail, memory_order_relaxed);
+	size_t curr_head = atomic_load_explicit(&r->head, memory_order_acquire);
+	if ((curr_tail - curr_head) >= logical_size)
+		return false;		//  buffer full
+	r->buf[curr_tail & r->mask] = byte;
+	atomic_store_explicit(&r->tail, curr_tail + 1, memory_order_release);
+	return true;
+}
+
+bool spsc_try_pop(t_spsc_ring *r, unsigned char *byte)
+{
+	size_t curr_head = atomic_load_explicit(&r->head, memory_order_relaxed);
+	size_t curr_tail = atomic_load_explicit(&r->tail, memory_order_acquire);
+	if (curr_head == curr_tail)
+		return false;		// buffer empty
+	(*byte) = r->buf[curr_head & r->mask];
+	atomic_store_explicit(&r->head, curr_head + 1, memory_order_release);
+	return true;
+}
+
+size_t spsc_push_batch(t_spsc_ring *r, const void *rawdata, size_t count)
 {
 
 }
 
-int spsc_try_push(t_spsc_ring *q, void *item)
-{
-
-}
-
-int spsc_try_pop(t_spsc_ring *q, void **item)
-{
-
-}
-
-size_t spsc_push_batch(t_spsc_ring *q, void **items, size_t count)
-{
-
-}
-
-size_t spsc_pop_batch(t_spsc_ring *q, void **items, size_t count)
+size_t spsc_pop_batch(t_spsc_ring *r, void *rawdata, size_t count)
 {
 
 }
--- spsc_ring/spsc_ring.h
+++ spsc_ring/spsc_ring.h
@@ -2,12 +2,13 @@
 #define SPSC_RING_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdalign.h>
 #include <stdatomic.h>
 
 // head and tail aligned as 64B 
 // to take a full cache line and avoid false sharing
-typedef struct spsc_ring
+struct spsc_ring
 {
 	alignas(64)
 	atomic_size_t head;
@@ -17,21 +18,21 @@ typedef struct spsc_ring
 	atomic_size_t tail;
 	char _tail_padding[64 - sizeof(atomic_size_t)];
 
-	void	**buffer;
-	size_t	capacity;
+	unsigned char	*buf;
+	size_t	size;
 	size_t	mask;
-}	t_spsc_ring;
-
+};
 
 // Minimal API
+typedef struct spsc_ring t_spsc_ring;
 
 t_spsc_ring *spsc_create(size_t size);
-void spsc_destroy(t_spsc_ring *q);
+void spsc_destroy(t_spsc_ring *r);
 
-int spsc_try_push(t_spsc_ring *q, void *item);
-int spsc_try_pop(t_spsc_ring *q, void **item);
+bool spsc_try_push(t_spsc_ring *r, unsigned char byte);
+bool spsc_try_pop(t_spsc_ring *r, unsigned char *byte);
 
-size_t spsc_push_batch(t_spsc_ring *q, void **items, size_t count);
-size_t spsc_pop_batch(t_spsc_ring *q, void **items, size_t count);
+size_t spsc_push_batch(t_spsc_ring *r, const void *rawdata, size_t count);
+size_t spsc_pop_batch(t_spsc_ring *r, void *rawdata, size_t count);
 
 #endif
