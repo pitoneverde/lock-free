8b7a8578a7418091486c934a398f45c037a840b9
pitoneverde <pitoneverde181@gmail.com>
2025-12-12 16:34:03 +0100

    added pseudocode

---
+++ ms_queue/ms_queue.c
@@ -0,0 +1,21 @@
+#include "ms_queue.h"
+
+t_ms_queue *create_ms_queue()
+{
+
+}
+
+void destroy_ms_queue(t_ms_queue *q)
+{
+
+}
+
+bool enqueue(t_ms_queue *q, void *data)
+{
+
+}
+
+void *dequeue(t_ms_queue *q)
+{
+
+}
